76281f636ca24be7e0f9a4aa26d6280c438fee38
Jukka Talvio <jukka.talvio@iki.fi>
2025-03-04 22:55:36 +0100

    User can choose which animals to list on the web page.

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -4,6 +4,7 @@ FOX_FILE = "animals_data.json"
 TEMPLATE_FILE = "animals_template.html"
 DATA_PLACEHOLDER = "__REPLACE_ANIMALS_INFO__"
 ANIMALS_FILE = "animals.html"
+ALL_SELECTION = "All of them"
 
 
 def load_data(file_path):
@@ -77,7 +78,7 @@ def serialize_animal(animal_obj):
     return animal_list_item
 
 
-def foxes_to_html_list(fox_file):
+def foxes_to_html_list(fox_file, skin_filter=ALL_SELECTION):
     """
     Read fox information and return it all in one string, each fox as HTML list (<li></li>) item
     :return: Information about foxes in one string format as HTML list items
@@ -85,7 +86,8 @@ def foxes_to_html_list(fox_file):
     foxes_data = load_data(fox_file)
     foxes_string = ""
     for fox in foxes_data:
-        foxes_string += serialize_animal(fox)
+        if skin_filter == ALL_SELECTION or fox.get('characteristics',{}).get('skin_type', None) == skin_filter:
+            foxes_string += serialize_animal(fox)
     return foxes_string
 
 
@@ -100,11 +102,76 @@ def save_to_file(file_path, data):
         file.write(data)
 
 
-def main():
-    template = read_template_file(TEMPLATE_FILE)
-    #print_foxes(FOX_FILE)
+def discover_skin_types(animal_data):
+    """
+    Discover what skin types the animal have
+    :param animal_data: animal_data in dict. Skin type is in a dict inside another dict
+    :return: list with all skin types
+    """
+    skin_types = set()
+    for animal in animal_data:
+       skin_types.add(animal.get('characteristics',{}).get('skin_type', None))
+    return list(skin_types)
 
-    fox_html_data = template.replace(DATA_PLACEHOLDER, foxes_to_html_list(FOX_FILE))
+
+def ask_number(message="Give a number: ", lower_limit=0, upper_limit=None, default_number=None, allow_empty=False):
+    """
+    Checks that the user enters a number and not something else
+    :param allow_empty: If yes, allows the user to not enter anything. In this case, returns None
+    :param default_number: Return this number if the user gives no other input except hits enter
+                           If default_number is None, empty answer is not allowed.
+                           The default_number needs to be a valid number as well.
+    :param upper_limit: The number from the user needs to be below or equal to this
+    :param lower_limit: The number from the user needs to be higher or equal to this
+    :param message: What we display to xplain to the user what number we need
+    :return: the number, duh! Or if allow_empty=True and user enters nothing, returns None
+    """
+    valid_number = False
+    while not valid_number:
+        if default_number is None:
+            user_input_number = input(f"{message}").strip()
+        else:
+            user_input_number = (input(f"{message}  [{default_number}] ").strip() or
+                                 default_number)
+        if len(user_input_number) == 0 and allow_empty:
+            return None
+        try:
+            user_input_number = float(user_input_number)
+            valid_number = True
+        except ValueError:
+            user_input_number = lower_limit - 1
+
+        if lower_limit is not None and user_input_number < lower_limit:
+            valid_number = False
+        if upper_limit is not None and user_input_number > upper_limit:
+            valid_number = False
+
+    return user_input_number
+
+
+def ask_which_skin_type(animal_file):
+    """
+    Ask the user which skin type we will use later
+    :return: One of the skin types from the animal_file data
+    """
+    skin_types = discover_skin_types(load_data(animal_file))
+    skin_types.sort()
+    skin_types.append(ALL_SELECTION)
+    for skin_num, skin_type in enumerate(skin_types):
+        print(f"{skin_num + 1:2} {skin_type}")
+    user_selection = int(ask_number(message="Which skin type you want to see in the web page? ",
+                                    lower_limit=1, upper_limit=len(skin_types)))
+    return skin_types[user_selection - 1]
+
+
+def main():
+    """
+    Create a HTML file ANIMALS_FILE which lists animals from the FOX_FILE.
+    :return: None
+    """
+    html_page_template = read_template_file(TEMPLATE_FILE)
+    skin_type = ask_which_skin_type(FOX_FILE)
+    fox_html_data = html_page_template.replace(DATA_PLACEHOLDER, foxes_to_html_list(FOX_FILE, skin_filter=skin_type))
     save_to_file(ANIMALS_FILE, fox_html_data)
 
 
